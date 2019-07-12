cdf597364d33189da5f3d828dbda17dc8ecf58b1
Sylvia Gilbert-Potts <sylvia.gilbert.potts@hotmail.com>
2019-07-12 12:09:32 +1200

    turn first dot blue

--- classes.js
+++ classes.js
@@ -5,8 +5,7 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+   one()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -17,6 +16,7 @@ function one () {
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
+
 
 // CREATE FUNCTION two HERE
 
