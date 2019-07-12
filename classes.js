825749b7fd66bb4adba6dbee1074e4b0bd0337ab
Sylvia Gilbert-Potts <sylvia.gilbert.potts@hotmail.com>
2019-07-12 12:14:08 +1200

    turn third circle purple

--- classes.js
+++ classes.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
    one()
    two()
+   three()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -21,6 +22,11 @@ function one () {
 function two () {
   var two = document.getElementById('two');
   two.classList.add('green');
+}
+
+function three () {
+  var three = document.getElementById('three');
+  three.classList.add('purple')
 }
 
 // CREATE FUNCTION two HERE
